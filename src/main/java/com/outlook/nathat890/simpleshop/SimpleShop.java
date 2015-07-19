5fbe45de72866d532c66aa443e4502564d1a80d2
BuildTools <unconfigured@null.spigotmc.org>
2015-07-19 11:00:15 +0100

    Initial commit

---
+++ src/main/java/com/outlook/nathat890/simpleshop/SimpleShop.java
@@ -0,0 +1,9 @@
+/* SimpleShop is a free plugin for servers
+   that need a Shop Plugin that is easy for their players to use
+   and for server owners to setup
+ */
+package com.outlook.nathat890.simpleshop;
+
+public class SimpleShop {
+
+}
